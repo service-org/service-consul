c99b6ad98d40349f8853ff96244a6400fcdd9877
forcemain <forcemain@163.com>
2021-08-04 11:30:41 +0800

    :underage:

---
+++ service_consul/cli/subctxs/consul.py
@@ -0,0 +1,25 @@
+#! -*- coding: utf-8 -*-
+#
+# author: forcemain@163.com
+
+from __future__ import annotations
+
+import typing as t
+
+from service_core.cli.subctxs import SubContext
+from service_core.core.configure import Configure
+from service_consul.core.proxy import ConsulProxy
+
+
+class Consul(SubContext):
+    """ 用于调试Consul接口 """
+
+    name: t.Text = 'consul'
+
+    def __init__(self, config: Configure) -> None:
+        """ 初始化实例
+
+        @param config: 配置对象
+        """
+        super(Consul, self).__init__(config)
+        self.proxy = ConsulProxy(config=config)
---
+++ service_consul/core/proxy.py
@@ -0,0 +1,39 @@
+#! -*- coding: utf-8 -*-
+#
+# author: forcemain@163.com
+
+from __future__ import annotations
+
+import typing as t
+
+from service_consul.core.consul import Consul
+from service_core.core.configure import Configure
+from service_consul.constants import CONSUL_CONFIG_KEY
+
+
+class ConsulProxy(object):
+    """ Consul代理类 """
+
+    def __init__(self, config: Configure, **options: t.Text) -> None:
+        """ 初始化实例
+
+        @param config: 配置对象
+        @param options: 其它选项
+        """
+        self.config = config
+        self.options = options
+
+    def __call__(self, alias: t.Text, **options: t.Text) -> Consul:
+        """
+
+        @param alias:
+        @param options:
+        @return:
+        """
+        cur_options = self.options
+        # 调用时传递的参数配置优先级最高
+        cur_options.update(options)
+        config = self.config.get(f'{CONSUL_CONFIG_KEY}.{alias}', default={})
+        # 调用时传递的参数配置优先级最高
+        config.update(cur_options)
+        return Consul(**config)

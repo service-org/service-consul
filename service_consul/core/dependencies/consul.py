2509db300ee2643de5f82cdbee3a75e6d383d4c1
李满满 <limanman@corp.netease.com>
2021-08-21 23:18:57 +0800

    :underage:

--- service_consul/core/dependencies/consul.py
+++ service_consul/core/dependencies/consul.py
@@ -22,18 +22,18 @@ class Consul(Dependency):
             self,
             alias: t.Text,
             data_center: t.Optional[t.Text] = None,
-            conn_options: t.Optional[t.Dict[t.Text, t.Any]] = None,
+            connect_options: t.Optional[t.Dict[t.Text, t.Any]] = None,
             **kwargs: t.Text) -> None:
         """ 初始化实例
 
         @param alias: 配置别名
         @param data_center: 数据中心
-        @param conn_options: 连接配置
+        @param connect_options: 连接配置
         """
         self.alias = alias
         self.client = None
         self.data_center = data_center or ''
-        self.conn_options = conn_options or {}
+        self.connect_options = connect_options or {}
         super(Consul, self).__init__(**kwargs)
 
     def setup(self) -> None:
@@ -43,5 +43,5 @@ class Consul(Dependency):
         """
         config = self.container.config.get(f'{CONSUL_CONFIG_KEY}.{self.alias}', default={})
         # 防止YAML中声明值为None
-        config = (config or {}) | self.conn_options
+        config = (config or {}) | self.connect_options
         self.client = ConsulClient(**config)

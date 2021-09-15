a4bbec76cf4ada28d0d0ec6089784fc4058ee9f4
forcemain <forcemain@163.com>
2021-09-15 13:40:38 +0800

    :underage:

--- service_consul/core/dependencies/consul.py
+++ service_consul/core/dependencies/consul.py
@@ -6,7 +6,7 @@ from __future__ import annotations
 
 import typing as t
 
-from service_consul.core.consul import ConsulClient
+from service_consul.core.client import ConsulClient
 from service_core.core.context import WorkerContext
 from service_consul.constants import CONSUL_CONFIG_KEY
 from service_core.core.service.dependency import Dependency
